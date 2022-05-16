f778e91f40b43c908c3e798a2a55e3551b541bfc
nijuy <youjin6325@naver.com>
2022-05-16 23:55:35 +0900

    추천 페이지 조건5(물주기) + 이벤트 추가

--- app/src/main/java/com/example/myapplication/recommend/FragmentRecommend.java
+++ app/src/main/java/com/example/myapplication/recommend/FragmentRecommend.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,20 +29,29 @@ public class FragmentRecommend extends Fragment {
         view = inflater.inflate(R.layout.fragment_recommend, container,false);
         Button btnGetResult;
         Button btnBack;
-        TextView result, cond1, cond2, cond3, cond4;
+        TextView result, cond1, cond2, cond3, cond4, cond5;
         RadioGroup group1, group2, group3, group4;
+        Spinner group5;
 
         btnBack = view.findViewById(R.id.btn_goSearch); //추천 페이지로 가는 버튼
         btnGetResult = view.findViewById(R.id.btn_getResult); // 조건 설정 다 하고 누르는 버튼
+
         result = view.findViewById(R.id.filteringCondition);
-        cond1 = view.findViewById(R.id.filteringCondition1);
+        cond1 = view.findViewById(R.id.filteringCondition1); // 조건 확인용 텍스트뷰라서 나중에 지우는게 좋을듯
         cond2 = view.findViewById(R.id.filteringCondition2);
         cond3 = view.findViewById(R.id.filteringCondition3);
         cond4 = view.findViewById(R.id.filteringCondition4);
+        cond5 = view.findViewById(R.id.filteringCondition5);
+
         group1 = view.findViewById(R.id.group1);
         group2 = view.findViewById(R.id.group2);
         group3 = view.findViewById(R.id.group3);
         group4 = view.findViewById(R.id.group4);
+        group5 = view.findViewById(R.id.group5);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.condition5_items, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        group5.setAdapter(adapter);
 
         btnBack.setOnClickListener(view -> {
             FragmentManager fm = getParentFragmentManager();
@@ -55,14 +67,19 @@ public class FragmentRecommend extends Fragment {
 
         btnGetResult.setOnClickListener(view -> {
 
-            if(group1.getCheckedRadioButtonId() == -1 | group2.getCheckedRadioButtonId() == -1 | group3.getCheckedRadioButtonId() == -1 | group4.getCheckedRadioButtonId() == -1){
+            if(group1.getCheckedRadioButtonId() == -1 |
+               group2.getCheckedRadioButtonId() == -1 |
+               group3.getCheckedRadioButtonId() == -1 |
+               group4.getCheckedRadioButtonId() == -1 |
+               group5.getSelectedItemPosition() == 0) {
                 Toast.makeText(view.getContext().getApplicationContext(), "선택하지 않은 조건이 있어요!", Toast.LENGTH_SHORT).show();
             } else {
-                result.setText("\n선택한 조건값 확인 :\n");
+                result.setText("선택한 조건값 확인 :\n");
                 result.append(cond1.getText());
                 result.append(cond2.getText());
                 result.append(cond3.getText());
                 result.append(cond4.getText());
+                result.append(cond5.getText());
             }
         });
 
@@ -150,6 +167,21 @@ public class FragmentRecommend extends Fragment {
             }
         }));
 
+        group5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                cond5.setText(Integer.toString(adapterView.getSelectedItemPosition())); // 선택된 항목의 위치
+                //cond5.setText(adapterView.getItemAtPosition(i).toString()); // 선택된 항목의 실제값
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         return view;
     }
+
+
 }
